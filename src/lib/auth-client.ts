454bf12a90a665a2eb3d65398813baeeeb148e5e
dasilva218 <dasilvawil218@gmail.com>
2026-02-10 17:29:31 +0100

    first projet

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,5 @@
+// import { createAuthClient } from "better-auth/client";
+
+import { createAuthClient } from "better-auth/react";
+
+export const { signIn, signUp, useSession } = createAuthClient()

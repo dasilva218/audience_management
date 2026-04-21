7ebb69a1ae8ecd085daf287026397cd5a007287e
dasilva218 <dasilvawil218@gmail.com>
2026-04-21 21:37:40 +0200

    ajout du provider ministry contexte

---
+++ src/context/ministryContextProvider.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { getMinistries } from "@/lib/action";
+import { MinistryType } from "@/lib/types/index_type";
+import { createContext, useContext, useEffect, useState } from "react";
+
+
+const contextType = {
+    ministries: [] as MinistryType[],
+    setMinistries: (ministries: MinistryType[]) => { }
+}
+
+const ministryContext = createContext<typeof contextType>(contextType)
+
+export const useMinistryContext = () => useContext<typeof contextType>(ministryContext)
+
+export default function MinistryContextProvider({ children }: { children: React.ReactNode }) {
+
+    const [ministries, setMinistries] = useState<MinistryType[]>([])
+
+    const fetchMinistries = async () => {
+        const res = await getMinistries()
+        setMinistries(res)
+    }
+
+    useEffect(() => {
+        fetchMinistries()
+    }, [])
+
+    return (
+        <ministryContext.Provider value={{ ministries, setMinistries }}>
+            {children}
+        </ministryContext.Provider>
+    );
+}
